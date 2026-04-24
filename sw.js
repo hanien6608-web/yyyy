7b47433a3b3d817596ab07f430ef55709f5f76c0
hanien6608-web <hanien6608@gmail.com>
2026-04-24 19:40:22 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -15,8 +15,12 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).catch(() => {
-      return caches.match(event.request);
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
     })
   );
 });
